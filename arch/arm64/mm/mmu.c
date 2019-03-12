c3b19c6b5958ef559ccca26aa84a6a4ef00605be
Yu Zhao <yuzhao@google.com>
2019-03-11 18:57:47 -0600

    arm64: mm: don't call page table ctors for init_mm
    init_mm doesn't require page table lock to be initialized at
    any level. Add a separate page table allocator for it, and the
    new one skips page table ctors.
    
    The ctors allocate memory when ALLOC_SPLIT_PTLOCKS is set. Not
    calling them avoids memory leak in case we call pte_free_kernel()
    on init_mm.
    
    Acked-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Yu Zhao <yuzhao@google.com>
    Signed-off-by: LibXZR <i@xzr.moe>

--- arch/arm64/mm/mmu.c
+++ arch/arm64/mm/mmu.c
@@ -406,6 +406,16 @@ static void __create_pgd_mapping(pgd_t *pgdir, phys_addr_t phys,
 	} while (pgdp++, addr = next, addr != end);
 }
 
+static phys_addr_t pgd_kernel_pgtable_alloc(int shift)
+{
+	void *ptr = (void *)__get_free_page(PGALLOC_GFP);
+	BUG_ON(!ptr);
+
+	/* Ensure the zeroed page is visible to the page table walker */
+	dsb(ishst);
+	return __pa(ptr);
+}
+
 static phys_addr_t pgd_pgtable_alloc(int shift)
 {
 	void *ptr = (void *)__get_free_page(PGALLOC_GFP);
@@ -641,7 +651,7 @@ static int __init map_entry_trampoline(void)
 	/* Map only the text into the trampoline page table */
 	memset(tramp_pg_dir, 0, PGD_SIZE);
 	__create_pgd_mapping(tramp_pg_dir, pa_start, TRAMP_VALIAS,
-			     entry_tramp_text_size(), prot, pgd_pgtable_alloc,
+			     entry_tramp_text_size(), prot, pgd_kernel_pgtable_alloc,
 			     0);
 
 	/* Map both the text and data into the kernel page table */
@@ -1116,7 +1126,8 @@ int arch_add_memory(int nid, u64 start, u64 size, struct vmem_altmap *altmap,
 		flags = NO_BLOCK_MAPPINGS | NO_CONT_MAPPINGS;
 
 	__create_pgd_mapping(swapper_pg_dir, start, __phys_to_virt(start),
-			     size, PAGE_KERNEL, pgd_pgtable_alloc, flags);
+			     size, PAGE_KERNEL, pgd_kernel_pgtable_alloc,
+			     flags);
 
 	return __add_pages(nid, start >> PAGE_SHIFT, size >> PAGE_SHIFT,
 			   altmap, want_memblock);

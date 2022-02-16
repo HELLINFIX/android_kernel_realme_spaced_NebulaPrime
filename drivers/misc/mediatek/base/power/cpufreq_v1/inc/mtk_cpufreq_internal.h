d71b80409c91025f93e757ca85f19b6f2a88c91d
Gagan Malvi <malvigagan@gmail.com>
2022-02-16 14:50:11 +0100

    mediatek: cpufreq_v1: Shim macros.
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- drivers/misc/mediatek/base/power/cpufreq_v1/inc/mtk_cpufreq_internal.h
+++ drivers/misc/mediatek/base/power/cpufreq_v1/inc/mtk_cpufreq_internal.h
@@ -89,10 +89,10 @@ extern unsigned int func_lv_mask;
 #define DEBUG 0
 
 #define TAG	"[Power/cpufreq] "
-#define tag_pr_notice(fmt, args...)	pr_debug(TAG fmt, ##args)
-#define tag_pr_info(fmt, args...)	pr_debug(TAG fmt, ##args)
-#define tag_pr_debug(fmt, args...)	pr_debug(TAG fmt, ##args)
-#define tag_pr_deferred(fmt, args...)   pr_debug(TAG fmt, ##args)
+#define tag_pr_notice(fmt, args...)		do {} while (0)
+#define tag_pr_info(fmt, args...)		do {} while (0)
+#define tag_pr_debug(fmt, args...)		do {} while (0)
+#define tag_pr_deferred(fmt, args...)   	do {} while (0)
 #define cpufreq_ver(fmt, args...)		\
 do {						\
 	if (func_lv_mask)			\

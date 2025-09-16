a35f76578c15edbe290ae572415179a87cda7c31
Cyber Knight <cyberknight755@gmail.com>
2025-09-16 18:37:07 +0800

    gen4m: Fix compilation on !CFG_MET_PACKET_TRACE_SUPPORT
    Change-Id: I46b172950baccd1d292c67d49ae5e2dbf13e5049
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/misc/mediatek/connectivity/wlan/core/gen4m/os/linux/include/gl_kal.h
+++ drivers/misc/mediatek/connectivity/wlan/core/gen4m/os/linux/include/gl_kal.h
@@ -1707,8 +1707,10 @@ int rx_thread(void *data);
 #endif
 uint64_t kalGetBootTime(void);
 
+#if CFG_MET_PACKET_TRACE_SUPPORT
 int kalMetInitProcfs(IN struct GLUE_INFO *prGlueInfo);
 int kalMetRemoveProcfs(void);
+#endif
 
 uint8_t kalGetEapolKeyType(void *prPacket);
 

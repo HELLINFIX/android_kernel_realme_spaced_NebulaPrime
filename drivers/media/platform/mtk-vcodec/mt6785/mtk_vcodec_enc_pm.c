c5d42878f6518df2e34440ab380ca8c26625dee6
HELLINFIX <mridulsharma57570@gmail.com>
2026-02-22 16:43:53 +0000

    oem: import changes for drivers/media

--- drivers/media/platform/mtk-vcodec/mt6785/mtk_vcodec_enc_pm.c
+++ drivers/media/platform/mtk-vcodec/mt6785/mtk_vcodec_enc_pm.c
@@ -13,23 +13,16 @@
 #include "mtk_vcodec_util.h"
 #include "mtk_vcu.h"
 #include "smi_public.h"
-#include "mt6833/smi_port.h"
+#include "mt6785/smi_port.h"
 
-#ifdef CONFIG_MTK_PSEUDO_M4U
-#include <mach/mt_iommu.h>
-#include "mach/pseudo_m4u.h"
-#endif
-
-#include "swpm_me.h"
-
-#if ENC_DVFS
-#include <linux/pm_qos.h>
+#ifdef ENC_DVFS
 #include <linux/soc/mediatek/mtk-pm-qos.h>
+#include <linux/pm_qos.h>
 #include <mmdvfs_pmqos.h>
 #include "vcodec_dvfs.h"
-#define STD_VENC_FREQ 249
-#define STD_LUMA_BW 70
-#define STD_CHROMA_BW 35
+#define STD_VENC_FREQ 364
+#define STD_LUMA_BW 100
+#define STD_CHROMA_BW 50
 static struct mtk_pm_qos_request venc_qos_req_f;
 static u64 venc_freq;
 static u32 venc_freq_step_size;
@@ -38,7 +31,7 @@ static struct codec_history *venc_hists;
 static struct codec_job *venc_jobs;
 #endif
 
-#if ENC_EMI_BW
+#ifdef ENC_EMI_BW
 #include <mtk_smi.h>
 static struct plist_head venc_rlist;
 static struct mm_qos_request venc_rcpu;
@@ -74,7 +67,7 @@ int mtk_vcodec_init_enc_pm(struct mtk_vcodec_dev *mtkdev)
 	dev = &pdev->dev;
 
 	pm->chip_node = of_find_compatible_node(NULL,
-		NULL, "mediatek,mt6833-vcodec-enc");
+		NULL, "mediatek,venc_gcon");
 	node = of_parse_phandle(dev->of_node, "mediatek,larb", 0);
 	if (!node) {
 		mtk_v4l2_err("no mediatek,larb found");
@@ -90,6 +83,25 @@ int mtk_vcodec_init_enc_pm(struct mtk_vcodec_dev *mtkdev)
 	pdev = mtkdev->plat_dev;
 	pm->dev = &pdev->dev;
 
+	pm->clk_MT_SCP_SYS_VDE = devm_clk_get(&pdev->dev, "MT_SCP_SYS_VDE");
+
+	if (IS_ERR(pm->clk_MT_SCP_SYS_VDE)) {
+		mtk_v4l2_err("[VCODEC][ERROR] Unable to devm_clk_get MT_SCP_SYS_VDE\n");
+		return PTR_ERR(pm->clk_MT_SCP_SYS_VDE);
+	}
+
+	pm->clk_MT_SCP_SYS_VEN = devm_clk_get(&pdev->dev, "MT_SCP_SYS_VEN");
+	if (IS_ERR(pm->clk_MT_SCP_SYS_VEN)) {
+		mtk_v4l2_err("[VCODEC][ERROR] Unable to devm_clk_get MT_SCP_SYS_VEN\n");
+		return PTR_ERR(pm->clk_MT_SCP_SYS_VEN);
+	}
+
+	pm->clk_MT_SCP_SYS_DIS = devm_clk_get(&pdev->dev, "MT_SCP_SYS_DIS");
+	if (IS_ERR(pm->clk_MT_SCP_SYS_DIS)) {
+		mtk_v4l2_err("[VCODEC][ERROR] Unable to devm_clk_get MT_SCP_SYS_DIS\n");
+		return PTR_ERR(pm->clk_MT_SCP_SYS_DIS);
+	}
+
 	pm->clk_MT_CG_VENC = devm_clk_get(&pdev->dev, "MT_CG_VENC");
 	if (IS_ERR(pm->clk_MT_CG_VENC)) {
 		mtk_v4l2_err("[VCODEC][ERROR] Unable to devm_clk_get MT_CG_VENC\n");
@@ -101,55 +113,27 @@ int mtk_vcodec_init_enc_pm(struct mtk_vcodec_dev *mtkdev)
 
 void mtk_vcodec_release_enc_pm(struct mtk_vcodec_dev *mtkdev)
 {
-	//free_all_bw(&venc_bw);
+#if ENC_EMI_BW
+#endif
 }
 
 void mtk_venc_deinit_ctx_pm(struct mtk_vcodec_ctx *ctx)
 {
+#ifdef ENC_DVFS
+	free_hist_by_handle(&ctx->id, &venc_hists);
+#endif
 }
 
 void mtk_vcodec_enc_clock_on(struct mtk_vcodec_ctx *ctx, int core_id)
 {
-#ifdef CONFIG_MTK_PSEUDO_M4U
-	int i, larb_port_num, larb_id;
-	struct M4U_PORT_STRUCT port;
-#endif
 #ifndef FPGA_PWRCLK_API_DISABLE
 	struct mtk_vcodec_pm *pm = &ctx->dev->pm;
 	int ret;
 
-	smi_bus_prepare_enable(SMI_LARB7, "VENC");
+	smi_bus_prepare_enable(SMI_LARB3, "VENC");
 	ret = clk_prepare_enable(pm->clk_MT_CG_VENC);
 	if (ret)
 		mtk_v4l2_err("clk_prepare_enable CG_VENC fail %d", ret);
-
-	set_swpm_venc_active(true);
-#endif
-
-#ifdef CONFIG_MTK_PSEUDO_M4U
-	time_check_start(MTK_FMT_ENC, core_id);
-	if (core_id == MTK_VENC_CORE_0) {
-		larb_port_num = SMI_LARB7_PORT_NUM;
-		larb_id = 7;
-	} else {
-		larb_port_num = 0;
-		larb_id = 0;
-		mtk_v4l2_err("invalid core_id %d", core_id);
-		time_check_end(MTK_FMT_ENC, core_id, 50);
-		return;
-	}
-
-	//enable 34bits port configs & sram settings
-	for (i = 0; i < larb_port_num; i++) {
-		port.ePortID = MTK_M4U_ID(larb_id, i);
-		port.Direction = 0;
-		port.Distance = 1;
-		port.domain = 0;
-		port.Security = 0;
-		port.Virtuality = 1;
-		m4u_config_port(&port);
-	}
-	time_check_end(MTK_FMT_ENC, core_id, 50);
 #endif
 }
 
@@ -157,16 +141,15 @@ void mtk_vcodec_enc_clock_off(struct mtk_vcodec_ctx *ctx, int core_id)
 {
 #ifndef FPGA_PWRCLK_API_DISABLE
 	struct mtk_vcodec_pm *pm = &ctx->dev->pm;
-	set_swpm_venc_active(false);
 	clk_disable_unprepare(pm->clk_MT_CG_VENC);
-	smi_bus_disable_unprepare(SMI_LARB7, "VENC");
+	smi_bus_disable_unprepare(SMI_LARB3, "VENC");
 #endif
 }
 
 
 void mtk_prepare_venc_dvfs(void)
 {
-#if ENC_DVFS
+#ifdef ENC_DVFS
 	int ret;
 
 	mtk_pm_qos_add_request(&venc_qos_req_f, PM_QOS_VENC_FREQ,
@@ -181,7 +164,7 @@ void mtk_prepare_venc_dvfs(void)
 
 void mtk_unprepare_venc_dvfs(void)
 {
-#if ENC_DVFS
+#ifdef ENC_DVFS
 	int freq_idx = 0;
 
 	freq_idx = (venc_freq_step_size == 0) ? 0 : (venc_freq_step_size - 1);
@@ -194,44 +177,37 @@ void mtk_unprepare_venc_dvfs(void)
 
 void mtk_release_pmqos(struct mtk_vcodec_ctx *ctx)
 {
-#if ENC_DVFS
+#ifdef ENC_DVFS
 	free_hist_by_handle(&ctx->id, &venc_hists);
 #endif
 }
 
 void mtk_prepare_venc_emi_bw(void)
 {
-#if ENC_EMI_BW
+#ifdef ENC_EMI_BW
 	plist_head_init(&venc_rlist);
-	mm_qos_add_request(&venc_rlist, &venc_rcpu, M4U_PORT_L7_VENC_RCPU);
-	mm_qos_add_request(&venc_rlist, &venc_rec, M4U_PORT_L7_VENC_REC);
-	mm_qos_add_request(&venc_rlist, &venc_bsdma, M4U_PORT_L7_VENC_BSDMA);
-	mm_qos_add_request(&venc_rlist, &venc_sv_comv,
-					M4U_PORT_L7_VENC_SV_COMV);
-	mm_qos_add_request(&venc_rlist, &venc_rd_comv,
-					M4U_PORT_L7_VENC_RD_COMV);
-	mm_qos_add_request(&venc_rlist, &venc_cur_luma,
-					M4U_PORT_L7_VENC_CUR_LUMA);
-	mm_qos_add_request(&venc_rlist, &venc_cur_chroma,
-					M4U_PORT_L7_VENC_CUR_CHROMA);
-	mm_qos_add_request(&venc_rlist, &venc_ref_luma,
-					M4U_PORT_L7_VENC_REF_LUMA);
-	mm_qos_add_request(&venc_rlist, &venc_ref_chroma,
-					M4U_PORT_L7_VENC_REF_CHROMA);
+	mm_qos_add_request(&venc_rlist, &venc_rcpu, SMI_VENC_RCPU);
+	mm_qos_add_request(&venc_rlist, &venc_rec, SMI_VENC_REC);
+	mm_qos_add_request(&venc_rlist, &venc_bsdma, SMI_VENC_BSDMA);
+	mm_qos_add_request(&venc_rlist, &venc_sv_comv, SMI_VENC_SV_COMV);
+	mm_qos_add_request(&venc_rlist, &venc_rd_comv, SMI_VENC_RD_COMV);
+	mm_qos_add_request(&venc_rlist, &venc_cur_luma, SMI_VENC_CUR_LUMA);
+	mm_qos_add_request(&venc_rlist, &venc_cur_chroma, SMI_VENC_CUR_CHROMA);
+	mm_qos_add_request(&venc_rlist, &venc_ref_luma, SMI_VENC_REF_LUMA);
+	mm_qos_add_request(&venc_rlist, &venc_ref_chroma, SMI_VENC_REF_CHROMA);
 #endif
 }
 
 void mtk_unprepare_venc_emi_bw(void)
 {
-#if ENC_EMI_BW
-	mm_qos_remove_all_request(&venc_rlist);
+#ifdef ENC_EMI_BW
 #endif
 }
 
 
 void mtk_venc_dvfs_begin(struct mtk_vcodec_ctx *ctx)
 {
-#if ENC_DVFS
+#ifdef ENC_DVFS
 	int target_freq = 0;
 	u64 target_freq_64 = 0;
 	struct codec_job *venc_cur_job = 0;
@@ -244,14 +220,20 @@ void mtk_venc_dvfs_begin(struct mtk_vcodec_ctx *ctx)
 					venc_hists);
 		target_freq_64 = match_freq(target_freq, &venc_freq_steps[0],
 					venc_freq_step_size);
-
-		if (target_freq_64 > 458)
-			target_freq_64 = 458;
+		if (ctx->dev->enc_cnt > 1) {
+			/* Reduce available time / increase freq */
+			target_freq = target_freq * 2;
+			if (ctx->enc_params.svp_mode > 0)
+				target_freq = 630;
+		}
+		if (ctx->enc_params.operationrate >= 120 &&
+			(target_freq_64 > 450 ||
+			ctx->q_data[MTK_Q_DATA_DST].fmt->fourcc ==
+				V4L2_PIX_FMT_H264))
+			target_freq_64 = 450;
 
 		if (target_freq > 0) {
-			venc_freq = target_freq;
-			if (venc_freq > target_freq_64)
-				venc_freq = target_freq_64;
+			venc_freq = target_freq_64;
 
 			venc_cur_job->mhz = (int)target_freq_64;
 			mtk_pm_qos_update_request(&venc_qos_req_f, target_freq_64);
@@ -265,17 +247,15 @@ void mtk_venc_dvfs_begin(struct mtk_vcodec_ctx *ctx)
 #endif
 }
 
-#if ENC_DVFS
 static int mtk_venc_get_exec_cnt(struct mtk_vcodec_ctx *ctx)
 {
-	return (int)((readl(ctx->dev->enc_reg_base[VENC_SYS] + 0x1030) &
+	return (int)((readl(ctx->dev->enc_reg_base[VENC_SYS] + 0x17C) &
 			0x7FFFFFFF) / 1000);
 }
-#endif
 
 void mtk_venc_dvfs_end(struct mtk_vcodec_ctx *ctx)
 {
-#if ENC_DVFS
+#ifdef ENC_DVFS
 	int freq_idx = 0;
 	long long interval = 0;
 	struct codec_job *venc_cur_job = 0;
@@ -285,6 +265,7 @@ void mtk_venc_dvfs_end(struct mtk_vcodec_ctx *ctx)
 	venc_cur_job = venc_jobs;
 	if (venc_cur_job != 0 && (venc_cur_job->handle == &ctx->id)) {
 		venc_cur_job->end = get_time_us();
+
 		if (ctx->enc_params.operationrate < 120) {
 			if (ctx->enc_params.framerate_denom == 0)
 				ctx->enc_params.framerate_denom = 1;
@@ -322,10 +303,6 @@ void mtk_venc_dvfs_end(struct mtk_vcodec_ctx *ctx)
 			} else {
 				update_hist(venc_cur_job, &venc_hists, 0);
 			}
-		} else if (ctx->enc_params.operationrate == 120) {
-			interval = (long long)(1000 * 1000 /
-					(int)ctx->enc_params.operationrate);
-			update_hist(venc_cur_job, &venc_hists, interval);
 		} else {
 			/* Set allowed time for slowmotion 4 buffer pack */
 			interval = (long long)(1000 * 1000 * 4 /
@@ -347,7 +324,7 @@ void mtk_venc_dvfs_end(struct mtk_vcodec_ctx *ctx)
 
 void mtk_venc_emi_bw_begin(struct mtk_vcodec_ctx *ctx)
 {
-#if ENC_EMI_BW
+#ifdef ENC_EMI_BW
 	int f_type = 1; /* TODO */
 	int boost_perc = 0;
 	int rcpu_bw = 5;
@@ -360,24 +337,25 @@ void mtk_venc_emi_bw_begin(struct mtk_vcodec_ctx *ctx)
 	int ref_luma_bw = 0;
 	int ref_chroma_bw = 0;
 
-	if (ctx->enc_params.operationrate >= 60)
+	if (ctx->enc_params.operationrate >= 120 ||
+		ctx->q_data[MTK_Q_DATA_DST].fmt->fourcc == V4L2_PIX_FMT_H265 ||
+		ctx->q_data[MTK_Q_DATA_DST].fmt->fourcc == V4L2_PIX_FMT_HEIF ||
+		(ctx->q_data[MTK_Q_DATA_SRC].visible_width == 3840 &&
+		ctx->q_data[MTK_Q_DATA_SRC].visible_height == 2160)) {
+		boost_perc = 100;
+	} else if (ctx->enc_params.operationrate == 60) {
 		boost_perc = 30;
+	}
 
 	/* Input BW scaling to venc_freq & config */
-#if BITS_PER_LONG == 32
-	cur_luma_bw = div_u64(STD_LUMA_BW * venc_freq * (100 + boost_perc),
-			 (STD_VENC_FREQ * 100));
-	cur_chroma_bw = div_u64(STD_CHROMA_BW * venc_freq * (100 + boost_perc),
-			 (STD_VENC_FREQ * 100));
-#else
 	cur_luma_bw = STD_LUMA_BW * venc_freq * (100 + boost_perc)
 			/ STD_VENC_FREQ / 100;
 	cur_chroma_bw = STD_CHROMA_BW * venc_freq * (100 + boost_perc)
 			/ STD_VENC_FREQ / 100;
-#endif
 
 	/* BW in venc_freq */
 	if (f_type == 0) {
+		/* I frame */
 		rec_bw = cur_luma_bw + cur_chroma_bw;
 	} else {
 		rec_bw = cur_luma_bw + cur_chroma_bw;
@@ -396,19 +374,9 @@ void mtk_venc_emi_bw_begin(struct mtk_vcodec_ctx *ctx)
 	mm_qos_set_request(&venc_rd_comv, rd_comv_bw, 0, BW_COMP_NONE);
 	mm_qos_set_request(&venc_cur_luma, cur_luma_bw, 0, BW_COMP_NONE);
 	mm_qos_set_request(&venc_cur_chroma, cur_chroma_bw, 0, BW_COMP_NONE);
-	if (1) { /* UFO */
-		mm_qos_set_request(&venc_rec, rec_bw, 0, BW_COMP_VENC);
-		mm_qos_set_request(&venc_ref_luma, ref_luma_bw, 0,
-				BW_COMP_VENC);
-		mm_qos_set_request(&venc_ref_chroma, ref_chroma_bw, 0,
-				BW_COMP_VENC);
-	} else {
-		mm_qos_set_request(&venc_rec, rec_bw, 0, BW_COMP_NONE);
-		mm_qos_set_request(&venc_ref_luma, ref_luma_bw, 0,
-				BW_COMP_NONE);
-		mm_qos_set_request(&venc_ref_chroma, ref_chroma_bw, 0,
-				BW_COMP_NONE);
-	}
+	mm_qos_set_request(&venc_rec, rec_bw, 0, BW_COMP_NONE);
+	mm_qos_set_request(&venc_ref_luma, ref_luma_bw, 0, BW_COMP_NONE);
+	mm_qos_set_request(&venc_ref_chroma, ref_chroma_bw, 0, BW_COMP_NONE);
 	mm_qos_update_all_request(&venc_rlist);
 #endif
 }
@@ -431,7 +399,7 @@ void mtk_venc_emi_bw_end(struct mtk_vcodec_ctx *ctx)
 
 void mtk_venc_pmqos_prelock(struct mtk_vcodec_ctx *ctx, int core_id)
 {
-#if ENC_DVFS
+#ifdef ENC_DVFS
 	mutex_lock(&ctx->dev->enc_dvfs_mutex);
 	add_job(&ctx->id, &venc_jobs);
 	mutex_unlock(&ctx->dev->enc_dvfs_mutex);
@@ -450,16 +418,15 @@ void mtk_venc_pmqos_end_frame(struct mtk_vcodec_ctx *ctx, int core_id)
 	mtk_venc_emi_bw_end(ctx);
 }
 
+/* Total job count after this one is inserted */
 void mtk_venc_pmqos_gce_flush(struct mtk_vcodec_ctx *ctx, int core_id,
-			int job_cnt)
+				int job_cnt)
 {
-
 }
 
+/* Remaining job count after this one is done */
 void mtk_venc_pmqos_gce_done(struct mtk_vcodec_ctx *ctx, int core_id,
-			int job_cnt)
+				int job_cnt)
 {
-
 }
-
 
--- drivers/media/platform/mtk-vcodec/mt6853/mtk_vcodec_enc_pm.c
+++ drivers/media/platform/mtk-vcodec/mt6853/mtk_vcodec_enc_pm.c
@@ -1,7 +1,8 @@
-/* SPDX-License-Identifier: GPL-2.0 */
+// SPDX-License-Identifier: GPL-2.0+
 /*
  * Copyright (c) 2019 MediaTek Inc.
-*/
+ */
+
 
 #include <linux/clk.h>
 #include <linux/of_address.h>
@@ -13,7 +14,7 @@
 #include "mtk_vcodec_util.h"
 #include "mtk_vcu.h"
 #include "smi_public.h"
-#include "mt6833/smi_port.h"
+#include "mt6853/smi_port.h"
 
 #ifdef CONFIG_MTK_PSEUDO_M4U
 #include <mach/mt_iommu.h>
@@ -74,7 +75,7 @@ int mtk_vcodec_init_enc_pm(struct mtk_vcodec_dev *mtkdev)
 	dev = &pdev->dev;
 
 	pm->chip_node = of_find_compatible_node(NULL,
-		NULL, "mediatek,mt6833-vcodec-enc");
+		NULL, "mediatek,mt6853-vcodec-enc");
 	node = of_parse_phandle(dev->of_node, "mediatek,larb", 0);
 	if (!node) {
 		mtk_v4l2_err("no mediatek,larb found");
@@ -157,6 +158,7 @@ void mtk_vcodec_enc_clock_off(struct mtk_vcodec_ctx *ctx, int core_id)
 {
 #ifndef FPGA_PWRCLK_API_DISABLE
 	struct mtk_vcodec_pm *pm = &ctx->dev->pm;
+
 	set_swpm_venc_active(false);
 	clk_disable_unprepare(pm->clk_MT_CG_VENC);
 	smi_bus_disable_unprepare(SMI_LARB7, "VENC");
@@ -245,7 +247,8 @@ void mtk_venc_dvfs_begin(struct mtk_vcodec_ctx *ctx)
 		target_freq_64 = match_freq(target_freq, &venc_freq_steps[0],
 					venc_freq_step_size);
 
-		if (target_freq_64 > 458)
+		if (ctx->enc_params.operationrate >= 120 &&
+			target_freq_64 > 458)
 			target_freq_64 = 458;
 
 		if (target_freq > 0) {
@@ -268,7 +271,7 @@ void mtk_venc_dvfs_begin(struct mtk_vcodec_ctx *ctx)
 #if ENC_DVFS
 static int mtk_venc_get_exec_cnt(struct mtk_vcodec_ctx *ctx)
 {
-	return (int)((readl(ctx->dev->enc_reg_base[VENC_SYS] + 0x1030) &
+	return (int)((readl(ctx->dev->enc_reg_base[VENC_SYS] + 0x17C) &
 			0x7FFFFFFF) / 1000);
 }
 #endif
@@ -322,10 +325,6 @@ void mtk_venc_dvfs_end(struct mtk_vcodec_ctx *ctx)
 			} else {
 				update_hist(venc_cur_job, &venc_hists, 0);
 			}
-		} else if (ctx->enc_params.operationrate == 120) {
-			interval = (long long)(1000 * 1000 /
-					(int)ctx->enc_params.operationrate);
-			update_hist(venc_cur_job, &venc_hists, interval);
 		} else {
 			/* Set allowed time for slowmotion 4 buffer pack */
 			interval = (long long)(1000 * 1000 * 4 /
@@ -360,21 +359,20 @@ void mtk_venc_emi_bw_begin(struct mtk_vcodec_ctx *ctx)
 	int ref_luma_bw = 0;
 	int ref_chroma_bw = 0;
 
-	if (ctx->enc_params.operationrate >= 60)
+	if (ctx->enc_params.operationrate >= 120 ||
+		ctx->q_data[MTK_Q_DATA_DST].fmt->fourcc == V4L2_PIX_FMT_H265 ||
+		(ctx->q_data[MTK_Q_DATA_SRC].visible_width == 3840 &&
+		ctx->q_data[MTK_Q_DATA_SRC].visible_height == 2160)) {
+		boost_perc = 100;
+	} else if (ctx->enc_params.operationrate == 60) {
 		boost_perc = 30;
+	}
 
 	/* Input BW scaling to venc_freq & config */
-#if BITS_PER_LONG == 32
-	cur_luma_bw = div_u64(STD_LUMA_BW * venc_freq * (100 + boost_perc),
-			 (STD_VENC_FREQ * 100));
-	cur_chroma_bw = div_u64(STD_CHROMA_BW * venc_freq * (100 + boost_perc),
-			 (STD_VENC_FREQ * 100));
-#else
 	cur_luma_bw = STD_LUMA_BW * venc_freq * (100 + boost_perc)
 			/ STD_VENC_FREQ / 100;
 	cur_chroma_bw = STD_CHROMA_BW * venc_freq * (100 + boost_perc)
 			/ STD_VENC_FREQ / 100;
-#endif
 
 	/* BW in venc_freq */
 	if (f_type == 0) {

d47f579e5b55af3983fdce054d0c90a623e1d63c
HELLINFIX <111268251+HELLINFIX@users.noreply.github.com>
2026-03-15 18:38:48 +0000

    Revert "kernel: sched: Disable oplus `walt` implementation"
    This reverts commit d99472440bee6d5fac8df782ecbf62f0234b41a2.

--- kernel/sched_assist/sched_assist_common.c
+++ kernel/sched_assist/sched_assist_common.c
@@ -2,8 +2,7 @@
 /*
  * Copyright (C) 2020 Oplus. All rights reserved.
  */
-int sysctl_slide_boost_enabled = 0;
-int sysctl_boost_task_threshold = 0;
+
 
 #include <linux/version.h>
 #include <linux/sched.h>

a0d3f5ab9fd68b531ec3717640192455d9529efb
HELLINFIX <mridulsharma57570@gmail.com>
2026-04-21 08:49:02 +0000

    sysctl: add a new variable for two_hundred

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -150,6 +150,7 @@ static unsigned long zero_ul;
 static unsigned long one_ul = 1;
 static unsigned long long_max = LONG_MAX;
 static int one_hundred = 100;
+static int two_hundred = 200;
 
 static int one_thousand = 1000;
 #if defined(OPLUS_FEATURE_SCHED_ASSIST) && defined(CONFIG_OPLUS_FEATURE_UXIO_FIRST)
